3c1b8460ee37cb962043b481c5b2c933cb029a47
Patrick Dunn <patrickdunn@Patricks-MacBook-Pro.local>
2023-01-11 12:36:02 +0000

    adds voting components with error handling

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
+    <div className="nav">
     <nav>
+      <Link to="/">Home</Link>
+      <span> | </span>
       <Link to="/reviews">Reviews</Link>
       <span> | </span>
       <Link to="/categories">Categories</Link>
       <span> | </span>
       <Link to="/users">Users</Link>
     </nav>
+    </div>
   );
 };
 
--- src/Components/voting.jsx
+++ src/Components/voting.jsx
@@ -3,17 +3,26 @@ import { addVotes } from "../utils/api";
 
 export default function Voting({ votes, review_id }) {
   const [votesChange, setvotesChange] = useState(0);
+  const [err, setErr] = useState(null);
 
   function upVote() {
-    addVotes(review_id, 1).then(() => {
-      setvotesChange((currVotesChange) => currVotesChange + 1);
+    setvotesChange((currVotesChange) => currVotesChange + 1);
+    addVotes(review_id, 1).catch((err) => {
+      setvotesChange((currVotesChange) => currVotesChange - 1);
+      setErr("Something went wrong, please try again");
     });
   }
 
   function downVote() {
-    addVotes(review_id, -1).then(() => {
-      setvotesChange((currVotesChange) => currVotesChange - 1);
+    setvotesChange((currVotesChange) => currVotesChange - 1);
+    addVotes(review_id, -1).catch((err) => {
+      setvotesChange((currVotesChange) => currVotesChange + 1);
+      setErr("Something went wrong, please try again");
     });
+  }
+
+  if (err) {
+    return <p>{err}</p>;
   }
 
   return (
